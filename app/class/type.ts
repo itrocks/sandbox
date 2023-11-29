a1d2210210cdaf90a3abdd5b6f300c7908e49fe1
Baptiste Pillot <baptiste@pillot.fr>
2023-11-29 18:10:44 +0100

    ADD I23 Class type objectOf and typeOf translators

--- app/class/type.ts
+++ app/class/type.ts
@@ -1,4 +1,13 @@
 
+const objectOf = (target: object|Type): object => (typeof target === 'object')
+	? target
+	: new target
+
 type Type = new(...args:any[])=>object
 
+const typeOf = (target: object|Type): Type => (typeof target === 'object')
+	? Object.getPrototypeOf(target).constructor
+	: target
+
 export default Type
+export { objectOf, Type, typeOf }
