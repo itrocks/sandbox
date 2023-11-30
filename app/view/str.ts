834c2a71e0858cf0ec02c228c930dc4c87701c8e
Baptiste Pillot <baptiste@pillot.fr>
2023-11-30 10:06:50 +0100

    STD I23 Functions defined as properties are not methods: avoid it

--- app/view/str.ts
+++ app/view/str.ts
@@ -3,12 +3,12 @@ import { lcFirst, toClass, toDisplay, toRoute, toVar, ucFirst } from './rename'
 class Str extends String
 {
 
-	lcFirst   = () => lcFirst(this.toString())
-	toClass   = () => toClass(this.toString())
-	toDisplay = () => toDisplay(this.toString())
-	toRoute   = () => toRoute(this.toString())
-	toVar     = () => toVar(this.toString())
-	ucFirst   = () => ucFirst(this.toString())
+	lcFirst()   { return lcFirst(this.toString()) }
+	toClass()   { return toClass(this.toString()) }
+	toDisplay() { return toDisplay(this.toString()) }
+	toRoute()   { return toRoute(this.toString()) }
+	toVar()     { return toVar(this.toString()) }
+	ucFirst()   { return ucFirst(this.toString()) }
 
 }
 
