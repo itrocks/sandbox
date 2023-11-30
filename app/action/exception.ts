46394258471ed8c4c1176c438af2c298a8737c83
Baptiste Pillot <baptiste@pillot.fr>
2023-11-30 10:06:01 +0100

    STD I23 exception should always inherit Error

--- app/action/exception.ts
+++ app/action/exception.ts
@@ -1,11 +1,12 @@
 
-class Exception
+class Exception extends Error
 {
 
 	response: Response
 
 	constructor(response: Response|string, status: number = 404)
 	{
+		super()
 		this.response = (typeof response === 'string')
 			? new Response(response, { status: status })
 			: response
