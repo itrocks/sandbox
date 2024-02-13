e4cfdcc497ce67792ff657867208ae06d9b5bd4d
Baptiste Pillot <baptiste@pillot.fr>
2024-02-13 19:22:56 +0100

    REF IJ4 table-edit simpler with div contenteditable

--- front/demo/table.ts
+++ front/demo/table.ts
@@ -1,15 +1,15 @@
-import ColumnReorderTable  from '../column-reorder-table.js'
-import FixTable            from '../fix-table.js'
-import InheritBackground   from '../fix-table-inherit-background.js'
-import InheritBorder       from '../fix-table-inherit-border.js'
-import { tableBySelector } from '../table.js'
-import TableEdit           from '../table-edit.js'
+import ColumnReorderTable        from '../column-reorder-table.js'
+import FixTable                  from '../fix-table.js'
+import FixTableInheritBackground from '../fix-table-inherit-background.js'
+import FixTableInheritBorder     from '../fix-table-inherit-border.js'
+import { tableBySelector }       from '../table.js'
+import TableEdit                 from '../table-edit.js'
 
 let tables = tableBySelector('table', { plugins: [
 	ColumnReorderTable,
 	FixTable,
-	InheritBackground,
-	InheritBorder,
+	FixTableInheritBackground,
+	FixTableInheritBorder,
 	TableEdit
 ]})
 
--- front/table-edit.ts
+++ front/table-edit.ts
@@ -1,106 +1,63 @@
 import Table from './table.js'
 
-let keyUpEvent:       () => void
-let selected:         HTMLTableCellElement
-let selectedCol:      HTMLTableColElement|HTMLTableCellElement
-let selectedColStyle: string
-let selectedStyle:    string
-
-function cellPosition(cell: HTMLTableCellElement)
-{
-	let count = 1
-	let previous = cell.previousElementSibling
-	while (previous) {
-		if ((previous.tagName === 'TD') || (previous.tagName === 'TH')) {
-			count ++
-		}
-		previous = previous.previousElementSibling
-	}
-	return count
-}
-
-function editStyle()
-{
-	const computedStyle = getComputedStyle(selected)
-	const editStyle: Partial<CSSStyleDeclaration> = {}
-	const selectedGroup = selected.parentElement?.parentElement?.tagName
-	const selectedRect  = selected.getBoundingClientRect()
-
-	// keep same padding when scrolling
-	editStyle.scrollPaddingLeft  = computedStyle.paddingLeft
-	editStyle.scrollPaddingRight = computedStyle.paddingRight
-	// body cells, sticky or not
-	if (selectedGroup === 'TBODY') {
-		editStyle.paddingBottom = (parseFloat(computedStyle.paddingBottom) + 1) + 'px'
-		editStyle.paddingTop    = (parseFloat(computedStyle.paddingTop) + 1) + 'px'
-		editStyle.position = (computedStyle.position === 'sticky') ? 'absolute' : 'relative'
-	}
-	// foot/head cell (all sticky)
-	else {
-		editStyle.height = selectedRect.height + 'px'
-		editStyle.left   = selectedRect.left + 'px'
-		editStyle.top    = selectedRect.top + 'px'
-	}
-	editStyle.width = selectedRect.width + 'px'
-
-	selectedCol.style.boxSizing = 'border-box'
-	selectedCol.style.maxWidth  = selectedRect.width + 'px'
-	selectedCol.style.minWidth  = selectedRect.width + 'px'
-	selectedCol.style.width     = selectedRect.width + 'px'
-
-	return editStyle
-}
-
-function topForeignRowCell(table: TableEdit, cell: HTMLTableCellElement)
-{
-	const position = cellPosition(cell)
-	const col = table.element.querySelectorAll(':scope > colgroup')[0]
-	if (col) {
-		return col.children[position - 1] as HTMLTableColElement
-	}
-	const sections: NodeListOf<HTMLTableSectionElement>
-		= table.element.querySelectorAll(':scope > tbody, :scope > tfoot, :scope > thead')
-	const cellTr = cell.closest('TR')
-	let foreignRow: HTMLTableRowElement|undefined
-	sections.forEach(section => {
-		if (foreignRow) return
-		let tr = section.firstElementChild as HTMLTableRowElement|null
-		if (!tr) return
-		if (cellTr === tr) {
-			tr = tr.nextElementSibling as HTMLTableRowElement|null
-		}
-		if (!tr) return
-		foreignRow = tr
-	})
-	return foreignRow?.children[position - 1] as HTMLTableCellElement ?? cell
-}
+let selected:      HTMLTableCellElement
+let selectedStyle: string
 
 function selectCell(table: TableEdit, cell: HTMLTableCellElement|undefined)
 {
-	if (cell === selected) return undefined
+	if (cell === selected) return
 	unselectCell(table)
 	if (!cell) return
-	selected    = cell
-	selectedCol = topForeignRowCell(table, selected)
+	selected = cell
 
-	selectedColStyle = selectedCol.getAttribute('style') ?? ''
-	selectedStyle    = selected.getAttribute('style') ?? ''
+	const computedStyle = getComputedStyle(selected)
+	selectedStyle = selected.getAttribute('style') ?? ''
 
-	Object.entries(editStyle()).forEach(([index, value]) => {
-		// @ts-ignore editStyle() returns Partial<CSSStyleDeclaration>
-		selected.style[index] = value
-	})
 	selected.setAttribute('contenteditable', '')
+	setTimeout(() => {
+		const offset = getSelection()?.anchorOffset
+		selected.removeAttribute('contenteditable')
+
+		selected.innerHTML = '<div contenteditable>' + selected.innerHTML + '</div>'
+		const editable = selected.firstElementChild as HTMLDivElement
+
+		if (computedStyle.verticalAlign === 'middle') {
+			editable.style.lineHeight = computedStyle.height
+		}
+		editable.style.boxSizing         = 'border-box'
+		editable.style.paddingBottom     = computedStyle.paddingBottom
+		editable.style.paddingLeft       = computedStyle.paddingLeft
+		editable.style.paddingRight      = computedStyle.paddingRight
+		editable.style.paddingTop        = computedStyle.paddingTop
+		editable.style.scrollPaddingLeft = computedStyle.paddingLeft
+
+		selected.classList.add('editing')
+		selected.style.padding = '0'
+
+		const range = document.createRange()
+		if (editable.firstChild && offset) {
+			range.setStart(editable.firstChild, offset)
+			range.setEnd(editable.firstChild, offset)
+		}
+		else {
+			range.selectNodeContents(editable)
+		}
+		getSelection()?.removeAllRanges()
+		getSelection()?.addRange(range)
+
+		editable.addEventListener('keyup', () => table.reset())
+	})
 }
 
 function unselectCell(table: TableEdit)
 {
 	if (!selected) return
-	selected.removeAttribute('contenteditable')
-	selected.removeEventListener('keyup', keyUpEvent)
-	selectedColStyle.length
-		? selectedCol.setAttribute('style', selectedColStyle)
-		: selectedCol.removeAttribute('style')
+	let innerHTML = (selected.firstElementChild as HTMLDivElement).innerHTML
+	if (innerHTML.endsWith('<br>')) {
+		innerHTML = innerHTML.substring(0, innerHTML.length - 4)
+	}
+	selected.classList.remove('editing')
+	selected.innerHTML = innerHTML
 	selectedStyle.length
 		? selected.setAttribute('style', selectedStyle)
 		: selected.removeAttribute('style')
@@ -110,32 +67,10 @@ function unselectCell(table: TableEdit)
 export default class TableEdit extends Table
 {
 
-	protected setEditStyle()
-	{
-		this.styleSheet.push(`
-			${this.selector} > * > tr > * {
-				cursor: text;
-			}
-			${this.selector} > * > tr > [contenteditable] {
-				box-sizing: border-box;
-				display: block;
-				overflow: hidden;
-			}
-			${this.selector} > tbody > tr {
-				position: relative;
-			}
-			${this.selector} > tfoot > tr > [contenteditable],
-			${this.selector} > thead > tr > [contenteditable] {
-				position: fixed;
-			}
-		`)
-	}
-
 	TableEdit()
 	{
-		this.setEditStyle()
 		this.addEventListener(this.element, 'mousedown', event => {
-			if (event.target instanceof Element)
+			if (!(event.target instanceof Element)) return
 			selectCell(this, event.target.closest('td, th') as HTMLTableCellElement|undefined)
 		})
 	}
