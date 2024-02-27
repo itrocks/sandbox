1778c4c4828b9c9438a2110571dc7ef92e335525
Baptiste Pillot <baptiste@pillot.fr>
2024-02-27 10:08:34 +0100

    REF IT3 MySQL connects only when needed: a constructor must be passive

--- app/dao/mysql.ts
+++ app/dao/mysql.ts
@@ -7,39 +7,44 @@ export default class Mysql implements Dao
 
 	connection?: Connection
 
-	constructor(config: { host: string, user: string, password: string, database: string })
+	constructor(protected config: { host: string, user: string, password: string, database: string })
+	{}
+
+	async connect()
 	{
-		const mariaDbConfig: ConnectionConfig = config
+		const mariaDbConfig = this.config as ConnectionConfig
 		mariaDbConfig.allowPublicKeyRetrieval = true
-		createConnection(mariaDbConfig).then(connection => this.connection = connection)
+		return createConnection(mariaDbConfig).then(connection => this.connection = connection)
 	}
 
-	async read<T extends object>(type: new() => T, id: bigint|string): Promise<T>
+	async read<T extends object>(type: new() => T, id: bigint | string): Promise<T>
 	{
-		if (!this.connection) {
-			throw 'Not connected'
-		}
-		const rows: object[] = await this.connection.query(
+		const connection = this.connection ?? await this.connect()
+		if (!connection) throw 'Not connected'
+
+		const rows: object[] = await connection.query(
 			'SELECT * FROM `' + storeOf(type) + '` WHERE id = ?',
 			[id]
 		)
+
 		return Object.assign(new type, rows[0])
 	}
 
 	async search<T extends object>(type: new() => T, search: object = {}): Promise<T[]>
 	{
-		if (!this.connection) {
-			throw 'Not connected'
-		}
+		const connection = this.connection ?? await this.connect()
+		if (!connection) throw 'Not connected'
+
 		let sql = Object.keys(search).map(name => '`' + name + '` = ?').join(', ')
 		let searchValues = Object.values(search)
 		if (sql.length) {
 			sql = ' WHERE ' + sql
 		}
-		const rows: object[] = await this.connection.query(
+		const rows: object[] = await connection.query(
 			'SELECT * FROM `' + storeOf(type) + '`' + sql,
 			searchValues
 		)
+
 		return rows.map(row => Object.assign(new type, row))
 	}
 
