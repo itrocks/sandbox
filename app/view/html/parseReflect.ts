8ffb8ba3475f7ee9f71620ff2d767afce4de4b44
Baptiste Pillot <baptiste@pillot.fr>
2024-03-12 15:40:54 +0100

    IMP IJ10 parseReflect: any reflection property or method may be called

--- app/view/html/parseReflect.ts
+++ app/view/html/parseReflect.ts
@@ -1,10 +1,16 @@
-import { properties } from '../../class/reflect'
+import ReflectClass    from '../../class/reflect'
+import ReflectProperty from '../../property/reflect'
 
 export default function parseReflect(variable: string, data: any)
 {
-	switch (variable) {
-		case '%properties':
-			return properties(data)
+	if (typeof data !== 'object') {
+		throw 'Could not parse ' + variable + ' for non-object ' + data
 	}
-	return '?'
+	if (!((data instanceof ReflectClass) || (data instanceof ReflectProperty))) {
+		data = new ReflectClass(data)
+	}
+	const value = data[variable.substring(1)]
+	return (typeof value === 'function')
+		? value.call(data)
+		: value
 }
