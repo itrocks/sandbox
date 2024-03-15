748e69e36edbcb57c8b8587b48bbb9f562eefc31
Baptiste Pillot <baptiste@pillot.fr>
2024-03-15 10:30:43 +0100

    ADD IJ10 The main app router accepts more mime types

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,7 @@ import Exception                           from './action/exception'
 import { needOf }                          from './action/need'
 import ActionRequest                       from './action/request'
 import { appPath }                         from './app'
+import mimeTypes                           from './mime'
 import { fastifyRequest, fastifyResponse } from './server/fastify'
 import Response                            from './server/response'
 
@@ -40,10 +41,13 @@ const server = fastify()
 server.get<{ Params: { [index: string]: string, '*': string } }>('/*', async (originRequest, finalResponse) =>
 {
 	const request = fastifyRequest(originRequest)
-	if (request.path === '/favicon.ico') return
-	if (request.path.endsWith('.css') && !request.path.includes('./')) {
-		const headers = { 'Content-Type': 'text/css; charset=utf-8' }
-		return fastifyResponse(finalResponse, new Response(await readFile(appPath + request.path, 'utf-8'), 200, headers))
+	const dot     = request.path.lastIndexOf('.')
+	if ((dot > request.path.length - 7) && !request.path.includes('./')) {
+		const mimeType = mimeTypes.get(request.path.substring(dot + 1))
+		if (mimeType) {
+			const headers = { 'Content-Type': mimeType + '; charset=utf-8' }
+			return fastifyResponse(finalResponse, new Response(await readFile(appPath + request.path, 'utf-8'), 200, headers))
+		}
 	}
 	let response: Response
 	try {
---
+++ app/mime.ts
@@ -0,0 +1,12 @@
+export const mimeTypes = new Map([
+	['css',   'text/css'],
+	['ico',   'image/vnd.microsoft.icon'],
+	['jpg',   'image/jpeg'],
+	['map',   'application/json'],
+	['png',   'image/png'],
+	['scss',  'text/css'],
+	['svg',   'image/svg+xml'],
+	['woff',  'font/woff'],
+	['woff2', 'font/woff2']
+])
+export default mimeTypes
