b60f97345bc5c4a63061ba7d3612c30fa7c37255
Baptiste Pillot <baptiste@pillot.fr>
2024-03-15 16:03:00 +0100

    ADD IJ10 Pixel-precision + edit display and style

--- app/action/builtIn/edit/edit.ts
+++ app/action/builtIn/edit/edit.ts
@@ -1,36 +1,30 @@
 import Action                    from '../../../action/action'
 import Need                      from '../../../action/need'
 import Request                   from '../../../action/request'
-import { displayOf }             from '../../../view/class/display'
-import { representativeValueOf } from '../../../view/class/representative'
+import dao                       from '../../../dao/dao'
+import Template                  from '../../../view/html/template'
+import { sep }                   from 'path'
 
 @Need('object')
 export default class Edit extends Action
 {
 
-	html(request: Request)
+	async html(request: Request)
 	{
-		const object     = request.object
-		const mainTitle = `Edit ${displayOf(object)} ${representativeValueOf(object)}`
-		const pageHead  = `<html lang="en">
-<head><meta charset="utf-8"><title>${mainTitle}</title></head>
-<body>`
-		const pageFoot  = `
-</body>
-</html>`
-
-		const head = `<article class="${displayOf(object)} edit" data-id="${object.id}">`
-		const foot = '</article>'
-
-		const title = `<h2>${mainTitle}</h2>`
-
-		return this.htmlResponse(pageHead + head + title + foot + pageFoot)
+		const template = new Template(request.object)
+		return this.htmlResponse(await template.parseFile(__dirname + sep + 'edit.html'))
 	}
 
-	json(request: Request)
+	async json(request: Request)
 	{
-		// the edit API will return data needed by object editors : e.g. links to auto-completion API
-		return this.jsonResponse(request.object)
+		if (request.objects.length === 1) {
+			return this.jsonResponse(request.object)
+		}
+		if (request.objects.length > 1) {
+			return this.jsonResponse(request.objects)
+		}
+		const objects = await dao.search(request.getType())
+		return this.jsonResponse(objects)
 	}
 
 }
