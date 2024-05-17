1193b79e1a9cfa48ceae3b5a721ffa649ebe676b
Baptiste Pillot <baptiste@pillot.fr>
2024-05-17 10:40:33 +0200

    ADD IJ13 xtarget allows adding scripts and css into xloaded templates

---
+++ front/app/edit.ts
@@ -0,0 +1,1 @@
+console.log('loaded edit.js')
--- front/xtarget.ts
+++ front/xtarget.ts
@@ -33,13 +33,45 @@ function responseTextFilter(responseText: string)
 	return (begin > 11) ? responseText.substring(begin, responseText.indexOf('<!--END-->', begin)) : responseText
 }
 
+function responseTextHead(responseText: string)
+{
+	const addHead     = document.createElement('head')
+	const position    = responseText.indexOf('>', responseText.indexOf('<head') + 5) + 1
+	addHead.innerHTML = responseText.substring(position, responseText.indexOf('</head>', position))
+
+	const head = document.head
+	for (const element of Array.from(addHead.children)) {
+		if (
+			(element instanceof HTMLLinkElement)
+			&& (element.getAttribute('rel') === 'stylesheet')
+			&& !head.querySelector('link[href="' + element.getAttribute('href') + '"]')
+		) {
+			head.append(element)
+		}
+		if (
+			(element instanceof HTMLScriptElement)
+			&& !head.querySelector('script[src="' + element.getAttribute('src') + '"]')
+		) {
+			const script = document.createElement('script')
+			for (const attributeName of element.getAttributeNames()) {
+				const attributeValue = element.getAttribute(attributeName)
+				if (!attributeValue) continue
+				script.setAttribute(attributeName, attributeValue)
+			}
+			head.append(script)
+		}
+	}
+}
+
 async function setResponse(response: Response, targetString: string)
 {
 	const target = targetString.startsWith('#')
 		? document.getElementById(targetString.substring(1))
 		: document.querySelector<HTMLElement>(targetString)
 	if (!target) return undefined
-	target.innerHTML = responseTextFilter(await response.text())
+	const text = await response.text()
+	responseTextHead(text)
+	target.innerHTML = responseTextFilter(text)
 	return target
 }
 
