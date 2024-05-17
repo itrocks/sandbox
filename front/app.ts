b15463f37cb06449b7646d30b9403ec82b1b624b
Baptiste Pillot <baptiste@pillot.fr>
2024-05-17 14:40:34 +0200

    REF IJ13 Compact app xTarget definition

--- front/app.ts
+++ front/app.ts
@@ -1,22 +1,13 @@
 import build                       from './build.js'
-import { Options }                 from './plugin.js'
 import { XTarget, XTargetElement } from './xtarget.js'
 import XTargetBeginEnd             from './xtarget-begin-end.js'
 import XTargetHead                 from './xtarget-head.js'
 import XTargetHeadersSize          from './xtarget-headers-size.js'
 
-const options: Partial<Options<XTarget>> = {
-	plugins: [
-		XTargetBeginEnd,
-		XTargetHead,
-		XTargetHeadersSize
-	]
-}
-
 build<XTargetElement>(
-	'a[target=main], a[target^="#"],'
-	+ ' button[type=submit][formtarget=main], button[type=submit][formtarget^="#"],'
-	+ ' form[target=main], form[target^="#"],'
-	+ ' input[type=submit][formtarget=main], input[type=submit][formtarget^="#"]',
-	element => new XTarget(element, options)
+	'a[target="main"], a[target^="#"],'
+	+ ' form[target="main"], form[target^="#"],'
+	+ ' form button[type="submit"][formtarget="main"], form button[type="submit"][formtarget^="#"],'
+	+ ' form input[type="submit"][formtarget="main"], form input[type="submit"][formtarget^="#"]',
+	element => new XTarget(element, { plugins: [XTargetBeginEnd, XTargetHead, XTargetHeadersSize] })
 )
