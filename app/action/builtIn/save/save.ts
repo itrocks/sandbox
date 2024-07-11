4afa8da9b1971d18281188a2f5a5682ea6a59316
Baptiste Pillot <baptiste@pillot.fr>
2024-07-11 12:24:14 +0200

    ADD IT9 Save build-in action + convert simple data to object typed values

--- app/action/builtIn/save/save.ts
+++ app/action/builtIn/save/save.ts
@@ -1,14 +1,41 @@
-import Action  from '../../../action/action'
-import Need    from '../../../action/need'
-import Request from '../../../action/request'
-import dump    from '../../../debug/dump'
+import Action       from '../../../action/action'
+import Need         from '../../../action/need'
+import Request      from '../../../action/request'
+import ReflectClass from '../../../class/reflect'
+import dao          from '../../../dao/dao'
+import dump         from '../../../debug/dump'
 
 @Need('?object')
 export default class Save extends Action
 {
 
+	dataToObject(object: { [index: string]: any }, data: { [index: string]: string })
+	{
+		const propertyTypes = new ReflectClass(object).propertyTypes
+		for (const propertyName in propertyTypes) {
+			const propertyType = propertyTypes[propertyName]
+			switch (propertyType) {
+				case 'bigint':
+					object[propertyName] = BigInt(data[propertyName])
+					break
+				case 'boolean':
+					object[propertyName] = !['', '0', 'false'].includes(data[propertyName])
+					break
+				case 'number':
+					object[propertyName] = Number(data[propertyName])
+					break
+				default:
+					object[propertyName] = data[propertyName]
+					break
+			}
+		}
+	}
+
 	html(request: Request)
 	{
+		this.dataToObject(request.object, request.request.data)
+		console.log(new ReflectClass(request.object).propertyTypes)
+		//dao.save(request.object)
 		return this.htmlResponse(`<html lang="en">
 <head><meta charset="utf-8"><title>HTML save</title></head>
 <body>${dump(request.object)}</body>
@@ -17,6 +44,8 @@ export default class Save extends Action
 
 	json(request: Request)
 	{
+		this.dataToObject(request.object, request.request.data)
+		//dao.save(request.object)
 		return this.jsonResponse(request.object)
 	}
 
