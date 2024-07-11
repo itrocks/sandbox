d56a6b55da4ef40d7f4fdef90b00534f9bb8ddaf
Baptiste Pillot <baptiste@pillot.fr>
2024-07-11 12:24:09 +0200

    IMP IT9 Compose all scripts + register @Uses for replacements + register @File for each default export class

--- app/class/compose.ts
+++ app/class/compose.ts
@@ -1,21 +1,21 @@
 import path         from 'path'
 import Route        from '../action/route'
 import { getRoute } from '../action/routes'
+import File         from '../class/file'
 import config       from '../config/compose'
 import Type         from './type'
-import { uses }     from './uses'
+import Uses         from './uses'
 
 type SubstitutionModule = { __esModule: true, default?: Type }
 
-function addRouteToModule(module: any, file: string)
+function applyFileToType(module: any, file: string)
 {
-	if (
-		(typeof module.default === 'function')
-		&& (module.default.toString().startsWith('class '))
-	) {
+	const type = module.default
+	if (((typeof type)[0] === 'f') && (type.toString()[0] === 'c')) {
+		File(file)(type)
 		const route = getRoute(file.slice(0, -3))
 		if (route) {
-			Route(route)(module.default)
+			Route(route)(type)
 		}
 	}
 	return module
@@ -32,7 +32,7 @@ const replacements = Object.fromEntries(
 
 const cache        = {} as { [type: string]: SubstitutionModule }
 const Module       = require('module')
-const superRequire = Module.prototype.require as (...args: any) => any
+const superRequire = Module.prototype.require as (...args: any) => typeof Module
 
 Module.prototype.require = function(file: string)
 {
@@ -41,24 +41,24 @@ Module.prototype.require = function(file: string)
 		file = this.path + (this.path.endsWith('/') ? '' : '/') + file
 	}
 	file = path.normalize(require.resolve(file))
-	// no replacement
-	let replacementFiles = replacements[file]
-	if (!replacementFiles) {
-		return addRouteToModule(superRequire.apply(this, arguments), file)
-	}
 	// from cache
 	if (cache[file]) {
 		return cache[file]
 	}
+	// no replacement
+	let replacementFiles = replacements[file]
+	if (!replacementFiles) {
+		return cache[file] = applyFileToType(superRequire.apply(this, arguments as unknown as any[]), file)
+	}
 	// require parent
-	const module = { __esModule: true } as SubstitutionModule
-	cache[file]  = module
-	const parentModule = superRequire.apply(this, arguments)
+	const module       = { __esModule: true } as SubstitutionModule
+	cache[file]        = module
+	const parentModule = applyFileToType(superRequire.apply(this, arguments as unknown as any[]), file)
 	// compose
 	if (typeof replacementFiles !== 'object') {
 		replacementFiles = [replacementFiles]
 	}
-	const replacementTypes = replacementFiles.map(file => superRequire.call(this, file).default as Type)
-	module.default = uses(parentModule.default, replacementTypes)
+	const replacementTypes = replacementFiles.map(file => this.require(file).default as Type)
+	module.default = Uses(...replacementTypes)(parentModule.default)
 	return module
 }
