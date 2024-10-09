680914748171083303387ddadc029150d69aaa10
Baptiste Pillot <baptiste@pillot.fr>
2024-10-09 13:24:38 +0200

    FIX IT20 auto-redirect not triggered from #notification + should not hide the notification

--- front/auto-redirect.ts
+++ front/auto-redirect.ts
@@ -1,2 +1,8 @@
 
-export default (element: HTMLAnchorElement) => setTimeout(() => element.click())
+export default (element: HTMLAnchorElement) => {
+	setTimeout(() => {
+		if (!element.closest('body')) return
+		element.dataset.stopPropagation = '1'
+		element.click()
+	})
+}
