2646899bc4e43dfb731eb6ee0bdd88d9c9df48ed
Baptiste Pillot <baptiste@pillot.fr>
2024-11-28 12:15:16 +0100

    ADD IT28 HasName core mixin test

---
+++ app/core/has-name.ts
@@ -0,0 +1,9 @@
+import Required from '../property/validate/required'
+
+export default class HasName
+{
+
+	@Required()
+	name = ''
+
+}
---
+++ app/demo-line.ts
@@ -0,0 +1,15 @@
+import Demo    from './demo'
+import Uses    from './class/uses'
+import Store   from './dao/store'
+import HasName from './core/has-name'
+
+@Store()
+@Uses(HasName)
+export default class DemoLine
+{
+
+	demo?: Demo
+
+	number = 1
+
+}
