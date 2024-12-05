5ae1662480bb0f66ba29ea5b9bcd2b377126c485
Baptiste Pillot <baptiste@pillot.fr>
2024-12-05 14:42:44 +0100

    IMP IT21 auto-redirect regular *click propagation still active

--- front/auto-redirect.ts
+++ front/auto-redirect.ts
@@ -1,5 +1,10 @@
 
-export default (element: HTMLAnchorElement) => {
-	element.addEventListener('click', event => event.stopPropagation())
-	setTimeout(() => element.click())
+export default function autoRedirect(element: HTMLAnchorElement)
+{
+	const click = (event: MouseEvent) => event.stopPropagation()
+	element.addEventListener('click', click)
+	setTimeout(() => {
+		element.click()
+		element.removeEventListener('click', click)
+	})
 }
