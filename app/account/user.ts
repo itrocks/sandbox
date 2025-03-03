f48dfbffbd837d060f36f145998413b92279cddf
Baptiste Pillot <baptiste@pillot.fr>
2025-03-03 15:21:36 +0100

    REF IT60 Use @itrocks/email-address

--- app/account/user.ts
+++ app/account/user.ts
@@ -1,14 +1,14 @@
 import { Representative } from '@itrocks/class-view'
+import { EmailAddress }   from '@itrocks/email-address'
 import { Required }       from '@itrocks/required'
 import { Store }          from '@itrocks/store'
-import { Email }          from '../property/validate/email'
 import Account            from './account'
 
 @Representative('email') @Store()
 export default class User extends Account
 {
 
-	@Email() @Required()
+	@EmailAddress() @Required()
 	email = ''
 
 }
